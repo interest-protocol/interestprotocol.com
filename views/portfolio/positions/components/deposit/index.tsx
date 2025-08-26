f3e69e87b57614a5b2db0190fece6b0a1ab74dc9
LuyandraBranco <luyandrabranco@gmail.com>
2025-08-26 05:16:09 +0100

    ✨ feat: Additional Info

--- views/portfolio/positions/components/deposit/index.tsx
+++ views/portfolio/positions/components/deposit/index.tsx
@@ -1,12 +1,35 @@
-import { Div } from '@stylin.js/elements';
+import { Button, Div } from '@stylin.js/elements';
 import { FC } from 'react';
 
+import AdditionalInfo from '../additional-info';
 import Input from './components/input';
 
 const Deposit: FC = () => (
-  <Div display="flex" flexDirection="column">
+  <Div display="flex" flexDirection="column" gap="0.5rem">
     <Input index={0} />
     <Input index={1} />
+    <AdditionalInfo
+      depositRatio="53.73% USDT / 46.27% USDC"
+      estimatedApr="68.18%"
+    />
+
+    <Button
+      border="none"
+      p="0.5rem 1rem"
+      height="3.5rem"
+      display="flex"
+      color="#002A78"
+      fontWeight="500"
+      fontSize="1rem"
+      background="#B4C5FF"
+      alignItems="center"
+      fontFamily="Inter"
+      cursor="pointer"
+      borderRadius="0.75rem"
+      justifyContent="center"
+    >
+      Connect Wallet
+    </Button>
   </Div>
 );
 
