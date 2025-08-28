6fe23015a7fe1a94a9042a521de7cece2b5e725d
LuyandraBranco <luyandrabranco@gmail.com>
2025-08-28 21:31:16 +0100

    🚨 fix: token icon on success modal

--- views/components/success-modal/success-modal-token-card.tsx
+++ views/components/success-modal/success-modal-token-card.tsx
@@ -1,10 +1,9 @@
-import { Network } from '@interest-protocol/interest-aptos-v2';
 import { Div, P } from '@stylin.js/elements';
 import { FC } from 'react';
 
 import { ChevronDoubleLeftSVG } from '@/components/svg';
 import TokenIcon from '@/components/token-icon';
-import { useNetwork } from '@/lib/aptos-provider/network/network.hooks';
+import { Network } from '@/constants';
 import { TokenStandard } from '@/lib/coins-manager/coins-manager.types';
 import { formatMoney } from '@/utils';
 
@@ -14,63 +13,60 @@ const SuccessModalTokenCard: FC<SuccessModalTokenCardProps> = ({
   to,
   from,
   withoutAmount,
-}) => {
-  const network = useNetwork<Network>();
-  return (
-    <Div
-      py="m"
-      px="s"
-      gap="s"
-      bg="surface"
-      display="flex"
-      borderRadius="xs"
-      justifyContent="center"
-    >
-      <Div display="flex" alignItems="center">
-        <TokenIcon
-          withBg
-          size="1rem"
-          network={network}
-          symbol={from.symbol}
-          rounded={from.standard === TokenStandard.COIN}
-        />
-        <P
-          alignItems="center"
-          textAlign="center"
-          color="onSurface"
-          display="flex"
-          ml="s"
-        >
-          {`${!withoutAmount ? formatMoney(+from?.value) : ''} ${from.symbol}`}
-        </P>
-      </Div>
-      <Div display="flex" alignItems="center" color="onSurface">
-        <ChevronDoubleLeftSVG
-          maxHeight="0.75rem"
-          maxWidth="0.75rem"
-          width="100%"
-        />
-      </Div>
-      <Div display="flex" alignItems="center">
-        <TokenIcon
-          withBg
-          size="1.1rem"
-          symbol={to.symbol}
-          network={network as Network}
-          rounded={to.standard === TokenStandard.COIN}
-        />
-        <P
-          alignItems="center"
-          textAlign="center"
-          color="onSurface"
-          display="flex"
-          ml="s"
-        >
-          {`${!withoutAmount ? formatMoney(+to?.value) : ''} ${to.symbol}`}
-        </P>
-      </Div>
+}) => (
+  <Div
+    py="m"
+    px="s"
+    gap="s"
+    bg="surface"
+    display="flex"
+    borderRadius="xs"
+    justifyContent="center"
+  >
+    <Div display="flex" alignItems="center">
+      <TokenIcon
+        withBg
+        size="1rem"
+        network={Network.MovementMainnet}
+        symbol={from.symbol}
+        rounded={from.standard === TokenStandard.COIN}
+      />
+      <P
+        alignItems="center"
+        textAlign="center"
+        color="onSurface"
+        display="flex"
+        ml="s"
+      >
+        {`${!withoutAmount ? formatMoney(+from?.value) : ''} ${from.symbol}`}
+      </P>
     </Div>
-  );
-};
+    <Div display="flex" alignItems="center" color="onSurface">
+      <ChevronDoubleLeftSVG
+        maxHeight="0.75rem"
+        maxWidth="0.75rem"
+        width="100%"
+      />
+    </Div>
+    <Div display="flex" alignItems="center">
+      <TokenIcon
+        withBg
+        size="1.1rem"
+        symbol={to.symbol}
+        network={Network.MovementMainnet}
+        rounded={to.standard === TokenStandard.COIN}
+      />
+      <P
+        alignItems="center"
+        textAlign="center"
+        color="onSurface"
+        display="flex"
+        ml="s"
+      >
+        {`${!withoutAmount ? formatMoney(+to?.value) : ''} ${to.symbol}`}
+      </P>
+    </Div>
+  </Div>
+);
 
 export default SuccessModalTokenCard;
