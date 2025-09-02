a3712cf6921b365188e65ba39185d4ece4238c89
LuyandraBranco <luyandrabranco@gmail.com>
2025-09-02 23:19:48 +0100

    🚨 fix: types

--- views/portfolio/positions/components/card/card.types.ts
+++ views/portfolio/positions/components/card/card.types.ts
@@ -1,14 +1,14 @@
-export type ImageValue = {
+export interface ImageValue {
   url: string;
   alt?: string;
-};
+}
 
 export type ValueType = string | number | ImageValue | null | undefined;
 
 export interface CardInfoProps<T extends Record<string, unknown>> {
   width?: string;
   title?: string;
-  headers: string[];
+  headers: ReadonlyArray<string>;
   keys: Array<keyof T>;
   values: T;
   isLoading: boolean;
