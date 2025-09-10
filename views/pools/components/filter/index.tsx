3ff78b6b5adc9b71e743a3fee016b8ec74a3355a
LuyandraBranco <luyandrabranco@gmail.com>
2025-09-10 18:10:44 +0100

    ✨ feat: filter component

--- views/pools/components/filter/index.tsx
+++ views/pools/components/filter/index.tsx
@@ -1,42 +1,41 @@
 import { Div } from '@stylin.js/elements';
-import { FC, useState } from 'react';
+import { FC } from 'react';
 
-const Filter: FC = () => {
-  const [interval, setInterval] = useState<'1W' | '1M' | '3M' | '1Y'>('1M');
-  return (
-    <Div
-      gap="0.5rem"
-      bg="#9CA3AF1A"
-      display="flex"
-      height="2.25rem"
-      width="11rem"
-      p="0.25rem 0.5rem"
-      borderRadius="9999rem"
-      border="1px solid #9CA3AF1A"
-    >
-      {(['1W', '1M', '3M', '1Y'] as const).map((intendedInterval) => (
-        <Div
-          display="flex"
-          fontSize="0.875rem"
-          alignItems="center"
-          borderRadius="9999rem"
-          key={intendedInterval}
-          padding="0.25rem 0.5rem"
-          onClick={() => setInterval(intendedInterval)}
-          color={interval === intendedInterval ? '#FFFFFF' : '#9CA3AF'}
-          pointerEvents={interval === intendedInterval ? 'none' : undefined}
-          backgroundColor={
-            interval === intendedInterval ? '#9CA3AF33' : 'transparent'
-          }
-          style={{
-            cursor: interval === intendedInterval ? 'default' : 'pointer',
-          }}
-        >
-          {intendedInterval}
-        </Div>
-      ))}
-    </Div>
-  );
-};
+import { FilterProps } from './filter.types';
+
+const Filter: FC<FilterProps> = ({ interval, setInterval, options }) => (
+  <Div
+    gap="0.5rem"
+    bg="#9CA3AF1A"
+    display="flex"
+    height="2.25rem"
+    width="11rem"
+    p="0.25rem 0.5rem"
+    borderRadius="9999rem"
+    border="1px solid #9CA3AF1A"
+  >
+    {options.map((intendedInterval) => (
+      <Div
+        display="flex"
+        fontSize="0.875rem"
+        alignItems="center"
+        borderRadius="9999rem"
+        key={intendedInterval}
+        padding="0.25rem 0.5rem"
+        onClick={() => setInterval(intendedInterval)}
+        color={interval === intendedInterval ? '#FFFFFF' : '#9CA3AF'}
+        pointerEvents={interval === intendedInterval ? 'none' : undefined}
+        backgroundColor={
+          interval === intendedInterval ? '#9CA3AF33' : 'transparent'
+        }
+        style={{
+          cursor: interval === intendedInterval ? 'default' : 'pointer',
+        }}
+      >
+        {intendedInterval}
+      </Div>
+    ))}
+  </Div>
+);
 
 export default Filter;
