8950131b425c9d34066ee29ebe2c87fa52723fd9
LuyandraBranco <luyandrabranco@gmail.com>
2025-09-11 19:18:44 +0100

    ✨ feat: curvePool page

---
+++ views/pools/curve-pool/index.tsx
@@ -0,0 +1,15 @@
+import { Div } from '@stylin.js/elements';
+import { FC } from 'react';
+
+const CurvePool: FC = () => {
+  return (
+    <Div
+      gap="1rem"
+      display="flex"
+      flexDirection="column"
+      mt={['1rem', '1rem', '1rem', '2.5rem']}
+    ></Div>
+  );
+};
+
+export default CurvePool;
