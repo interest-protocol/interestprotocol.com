5f70b00d1bea069a62b8241837af9b487f3b43aa
Luyandra Branco <94245234+LuyandraBranco@users.noreply.github.com>
2025-10-06 20:14:34 +0100

    🐛 hotfix: Mobile device

--- views/pools/pools-table-mobile/index.tsx
+++ views/pools/pools-table-mobile/index.tsx
@@ -5,6 +5,7 @@ import Skeleton from 'react-loading-skeleton';
 import { v4 } from 'uuid';
 
 import { Button } from '@/components/button';
+import NoCoin from '@/components/layout/header/wallet/profile/menu-profile/tabs/no-coin';
 import PoolName from '@/components/pool-name';
 import SquarePlus from '@/components/svg/square-plus';
 import { Routes, RoutesEnum } from '@/constants';
@@ -41,9 +42,11 @@ const PoolsTableMobile: FC = () => {
     {} as Record<string, PoolMetrics>
   );
 
-  return isLoading ? (
-    <TableMobileSkeleton />
-  ) : (
+  if (isLoading) return <TableMobileSkeleton />;
+
+  const noPools = !POOLS.length;
+
+  return (
     <Div
       width="100%"
       bg="#030712"
@@ -51,118 +54,124 @@ const PoolsTableMobile: FC = () => {
       flexDirection="column"
       borderRadius="0.5rem"
       borderColor="#1F2937"
-      borderWidth="1px 1px 0px 1px"
       display={['flex', 'flex', 'flex', 'none']}
+      borderWidth={noPools ? '1px 1px 1px 1px' : '1px 1px 0px 1px'}
+      py={noPools ? ['1.5rem', '1.5rem', '1.5rem', 0] : undefined}
+      height={noPools ? ['11.75rem', '11.75rem', '11.75rem', 0] : undefined}
     >
-      {POOLS.map(({ poolAddress, tokensAddresses }) => {
-        const pool = poolsMetricsMap?.[poolAddress];
-        const isLoadingPool = !pool;
+      {noPools ? (
+        <NoCoin />
+      ) : (
+        POOLS.map(({ poolAddress, tokensAddresses }) => {
+          const pool = poolsMetricsMap?.[poolAddress];
+          const isLoadingPool = !pool;
 
-        return (
-          <Div
-            key={v4()}
-            p="1rem"
-            display="flex"
-            flexDirection="column"
-            gap="0.5rem"
-            borderStyle="solid"
-            borderColor="#1F2937"
-            borderWidth="0px 0px 1px 0px"
-          >
-            <Div display="flex" alignItems="center" gap="0.5rem">
-              <PoolName
-                address={poolAddress}
-                symbols={pool?.symbols}
-                tokensAddresses={pool?.coins ?? tokensAddresses}
-              />
-            </Div>
-            {isLoadingPool ? (
-              <>
-                <Skeleton width="100%" height="1rem" />
-                <Skeleton width="100%" height="1rem" />
-                <Skeleton width="100%" height="1rem" />
-              </>
-            ) : (
-              <>
-                <TableMobileLine
-                  label="TVL"
-                  value={formatDollars(Number(pool.metrics.tvl))}
-                />
-                <TableMobileLine
-                  label="Volume"
-                  value={formatDollars(Number(pool.metrics.volume))}
-                />
-                <TableMobileLine
-                  label="APR"
-                  value={
-                    <OverviewTooltip
-                      title={
-                        pool
-                          ? `${+(Number(pool.metrics.apr) + Number(pool.metrics.farmApr)).toFixed(2)}%`
-                          : 'Loading...'
-                      }
-                      feesApr={pool ? Number(pool.metrics.apr) : 0}
-                      rewardsApr={pool ? Number(pool.metrics.farmApr) : 0}
-                      apr={
-                        pool
-                          ? Number(pool.metrics.apr) +
-                            Number(pool.metrics.farmApr)
-                          : 0
-                      }
-                    />
-                  }
-                />
-              </>
-            )}
+          return (
             <Div
-              mt="0.5rem"
+              key={v4()}
+              p="1rem"
+              display="flex"
+              flexDirection="column"
               gap="0.5rem"
-              display="grid"
-              gridTemplateColumns="1fr 1fr"
+              borderStyle="solid"
+              borderColor="#1F2937"
+              borderWidth="0px 0px 1px 0px"
             >
-              <Button
-                px="1rem"
-                display="flex"
-                variant="tonal"
-                color="#B4C5FF"
-                fontSize="0.875rem"
-                lineHeight="1.25rem"
-                borderRadius="0.65rem"
-                borderColor="#B4C5FF"
-                justifyContent="center"
-                onClick={() =>
-                  PoolOverview({
-                    apr: pool
-                      ? `${(Number(pool.metrics.apr) + Number(pool.metrics.farmApr)).toFixed(2)}%`
-                      : '0',
-                    address: poolAddress,
-                    symbols: pool?.symbols,
-                    tokensAddresses: pool?.coins ?? tokensAddresses,
-                  })
-                }
-              >
-                Overview
-              </Button>
-              <Link
-                href={`${Routes[RoutesEnum.PoolDetails]}?address=${poolAddress}`}
-                title="pool name"
+              <Div display="flex" alignItems="center" gap="0.5rem">
+                <PoolName
+                  address={poolAddress}
+                  symbols={pool?.symbols}
+                  tokensAddresses={pool?.coins ?? tokensAddresses}
+                />
+              </Div>
+              {isLoadingPool ? (
+                <>
+                  <Skeleton width="100%" height="1rem" />
+                  <Skeleton width="100%" height="1rem" />
+                  <Skeleton width="100%" height="1rem" />
+                </>
+              ) : (
+                <>
+                  <TableMobileLine
+                    label="TVL"
+                    value={formatDollars(Number(pool.metrics.tvl))}
+                  />
+                  <TableMobileLine
+                    label="Volume"
+                    value={formatDollars(Number(pool.metrics.volume))}
+                  />
+                  <TableMobileLine
+                    label="APR"
+                    value={
+                      <OverviewTooltip
+                        title={
+                          pool
+                            ? `${+(Number(pool.metrics.apr) + Number(pool.metrics.farmApr)).toFixed(2)}%`
+                            : 'Loading...'
+                        }
+                        feesApr={pool ? Number(pool.metrics.apr) : 0}
+                        rewardsApr={pool ? Number(pool.metrics.farmApr) : 0}
+                        apr={
+                          pool
+                            ? Number(pool.metrics.apr) +
+                              Number(pool.metrics.farmApr)
+                            : 0
+                        }
+                      />
+                    }
+                  />
+                </>
+              )}
+              <Div
+                mt="0.5rem"
+                gap="0.5rem"
+                display="grid"
+                gridTemplateColumns="1fr 1fr"
               >
                 <Button
                   px="1rem"
-                  width="fill-available"
-                  variant="filled"
+                  display="flex"
+                  variant="tonal"
+                  color="#B4C5FF"
                   fontSize="0.875rem"
                   lineHeight="1.25rem"
                   borderRadius="0.65rem"
+                  borderColor="#B4C5FF"
+                  justifyContent="center"
+                  onClick={() =>
+                    PoolOverview({
+                      apr: pool
+                        ? `${(Number(pool.metrics.apr) + Number(pool.metrics.farmApr)).toFixed(2)}%`
+                        : '0',
+                      address: poolAddress,
+                      symbols: pool?.symbols,
+                      tokensAddresses: pool?.coins ?? tokensAddresses,
+                    })
+                  }
                 >
-                  Add Liquidity
-                  <SquarePlus maxWidth="1rem" maxHeight="1rem" width="100%" />
+                  Overview
                 </Button>
-              </Link>
+                <Link
+                  href={`${Routes[RoutesEnum.PoolDetails]}?address=${poolAddress}`}
+                  title="pool name"
+                >
+                  <Button
+                    px="1rem"
+                    width="fill-available"
+                    variant="filled"
+                    fontSize="0.875rem"
+                    lineHeight="1.25rem"
+                    borderRadius="0.65rem"
+                  >
+                    Add Liquidity
+                    <SquarePlus maxWidth="1rem" maxHeight="1rem" width="100%" />
+                  </Button>
+                </Link>
+              </Div>
             </Div>
-          </Div>
-        );
-      })}
+          );
+        })
+      )}
     </Div>
   );
 };
