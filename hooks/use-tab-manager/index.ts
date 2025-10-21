a0490cd445f3190fbc63cd4356a7aa41fd8c9c39
LuyandraBranco <luyandrabranco@gmail.com>
2025-10-21 18:36:02 +0100

    🐛 fix: tabs change

--- hooks/use-tab-manager/index.ts
+++ hooks/use-tab-manager/index.ts
@@ -1,21 +1,19 @@
 import { create } from 'zustand';
-
 interface TabState {
   tab: number;
   setTab: (tab: number) => void;
   innerTabs: Record<string, number>;
   setInnerTab: (key: string, tab: number) => void;
+  resetTabs: () => void;
 }
 
 export const useTabState = create<TabState>((set) => ({
   tab: 0,
-  setTab: (tab: number) => set({ tab }),
+  setTab: (tab) => set({ tab }),
   innerTabs: {},
-  setInnerTab: (key: string, tab: number) =>
+  setInnerTab: (key, tab) =>
     set((state) => ({
-      innerTabs: {
-        ...state.innerTabs,
-        [key]: tab,
-      },
+      innerTabs: { ...state.innerTabs, [key]: tab },
     })),
+  resetTabs: () => set({ tab: 0, innerTabs: {} }),
 }));
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,43 +9,48 @@ import { SkeletonTheme } from 'react-loading-skeleton';
 
 import ModalProvider from '@/components/modal-provider';
 import { INDEXER_URL, Network, RPC_URL, TOAST_DURATION } from '@/constants';
+import { useResetTabs } from '@/hooks/use-reset-tabs';
 import { AptosProvider } from '@/lib/aptos-provider';
 import { GlobalStyles } from '@/styles';
 
-const MyApp = ({ Component, pageProps }: AppProps<NextPage>): ReactNode => (
-  <AptosProvider
-    defaultNetwork={Network.MAINNET}
-    networks={[
-      {
-        network: Network.MAINNET,
-        rpc: RPC_URL[Network.MAINNET],
-        indexer: INDEXER_URL[Network.MAINNET],
-      },
-    ]}
-  >
-    <SkeletonTheme baseColor="#99BBFF28" highlightColor="#99BBFF14">
-      <StrictMode>
-        <Global styles={GlobalStyles} />
-        <ModalProvider />
-        <Toaster
-          position="bottom-right"
-          toastOptions={{
-            duration: TOAST_DURATION,
-            style: {
-              zIndex: 100,
-              maxWidth: '20rem',
-              overflow: 'hidden',
-              position: 'relative',
-              background: '#242C32',
-              boxShadow:
-                '0px 16px 24px 0px rgba(0, 0, 0, 0.14), 0px 6px 30px 0px rgba(0, 0, 0, 0.12), 0px 8px 10px 0px rgba(0, 0, 0, 0.20)',
-            },
-          }}
-        />
-        <Component {...pageProps} />
-      </StrictMode>
-    </SkeletonTheme>
-  </AptosProvider>
-);
+const MyApp = ({ Component, pageProps }: AppProps<NextPage>): ReactNode => {
+  useResetTabs();
+
+  return (
+    <AptosProvider
+      defaultNetwork={Network.MAINNET}
+      networks={[
+        {
+          network: Network.MAINNET,
+          rpc: RPC_URL[Network.MAINNET],
+          indexer: INDEXER_URL[Network.MAINNET],
+        },
+      ]}
+    >
+      <SkeletonTheme baseColor="#99BBFF28" highlightColor="#99BBFF14">
+        <StrictMode>
+          <Global styles={GlobalStyles} />
+          <ModalProvider />
+          <Toaster
+            position="bottom-right"
+            toastOptions={{
+              duration: TOAST_DURATION,
+              style: {
+                zIndex: 100,
+                maxWidth: '20rem',
+                overflow: 'hidden',
+                position: 'relative',
+                background: '#242C32',
+                boxShadow:
+                  '0px 16px 24px 0px rgba(0, 0, 0, 0.14), 0px 6px 30px 0px rgba(0, 0, 0, 0.12), 0px 8px 10px 0px rgba(0, 0, 0, 0.20)',
+              },
+            }}
+          />
+          <Component {...pageProps} />
+        </StrictMode>
+      </SkeletonTheme>
+    </AptosProvider>
+  );
+};
 
 export default MyApp;
