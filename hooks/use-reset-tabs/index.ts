5047ca13295aa810783e6f8e947688f75d3e8a50
LuyandraBranco <luyandrabranco@gmail.com>
2025-10-21 20:14:52 +0100

    ♻️ refactor: call useresettabs

--- hooks/use-reset-tabs/index.ts
+++ hooks/use-reset-tabs/index.ts
@@ -3,11 +3,11 @@ import { useEffect } from 'react';
 
 import { useTabState } from '../use-tab-manager';
 
-export function useResetTabs() {
+export const useResetTabs = () => {
   const pathname = usePathname();
   const { resetTabs } = useTabState();
 
   useEffect(() => {
     resetTabs();
   }, [pathname]);
-}
+};
