63a58fe952100b23c6d06f22ad2da0fc6cddc7df
Marco <marcomorete73@hotmail.com>
2025-10-26 01:05:58 +0100

    :sparkles: feat: add switch wallet feature

--- components/wallet-guard-button/index.tsx
+++ components/wallet-guard-button/index.tsx
@@ -1,9 +1,12 @@
 import { useAptosWallet } from '@razorlabs/wallet-kit';
 import { Button, ButtonProps } from '@stylin.js/elements';
+import { motion } from 'motion/react';
 import { isEmpty } from 'ramda';
 import { FC } from 'react';
 
 import { useConnectWalletModal } from './connect-wallet.hook';
+
+const MotionButton = motion(Button);
 
 const WalletGuardButton: FC<ButtonProps> = ({ children, ...props }) => {
   const connectWalletModal = useConnectWalletModal();
@@ -11,43 +14,67 @@ const WalletGuardButton: FC<ButtonProps> = ({ children, ...props }) => {
 
   if (status === 'connecting' || connecting)
     return (
-      <Button
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      <MotionButton
+        layout
         all="unset"
         px="2rem"
         py="1rem"
-        display="flex"
         bg="#B4C5FF"
+        display="flex"
         cursor="pointer"
         fontWeight="500"
         textAlign="center"
         borderRadius="0.75rem"
         justifyContent="center"
+        initial={{
+          background: '#9CA3AF1A',
+          borderColor: '#9CA3AF1A',
+        }}
+        animate={{
+          background: '#B4C5FF',
+          borderColor: 'transparent',
+          transition: { duration: 0.5 },
+        }}
         {...props}
         onClick={undefined}
         disabled
       >
         Connecting...
-      </Button>
+      </MotionButton>
     );
 
   if (isEmpty(address) || !account || isEmpty(account?.address))
     return (
-      <Button
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      <MotionButton
+        layout
         all="unset"
         px="2rem"
         py="1rem"
         display="flex"
-        bg="#B4C5FF"
+        border="1px solid"
         fontWeight="500"
         cursor="pointer"
         textAlign="center"
         borderRadius="0.75rem"
         justifyContent="center"
+        initial={{
+          background: '#9CA3AF1A',
+          borderColor: '#9CA3AF1A',
+        }}
+        animate={{
+          background: '#B4C5FF',
+          borderColor: 'transparent',
+          transition: { duration: 0.5 },
+        }}
         {...props}
         onClick={connectWalletModal}
       >
         Connect Wallet
-      </Button>
+      </MotionButton>
     );
 
   return children;
--- lib/aptos-provider/wallet/index.tsx
+++ lib/aptos-provider/wallet/index.tsx
@@ -1,10 +1,33 @@
-import { AptosWalletProvider } from '@razorlabs/wallet-kit';
-import { type FC, type PropsWithChildren } from 'react';
+import { AptosWalletProvider, useAptosWallet } from '@razorlabs/wallet-kit';
+import { type FC, type PropsWithChildren, useEffect, useState } from 'react';
 
 import { CUSTOM_WALLETS } from '@/components/layout/header/wallet/connect-wallet/connect-wallet.data';
 
+const WalletManager: FC = () => {
+  const [wallet, setWallet] = useState<string | null>(null);
+  const { account, adapter, disconnect, select, name } = useAptosWallet();
+
+  useEffect(() => {
+    adapter?.features['aptos:onAccountChange'].onAccountChange((newAccount) => {
+      if (newAccount.address.toString() === account?.address) return;
+
+      disconnect().then(() => setWallet(name as string));
+    });
+  }, [adapter]);
+
+  useEffect(() => {
+    if (wallet) {
+      select('Nightly');
+      setWallet(null);
+    }
+  }, [wallet]);
+
+  return null;
+};
+
 export const WalletProvider: FC<PropsWithChildren> = ({ children }) => (
   <AptosWalletProvider defaultWallets={Array.from(CUSTOM_WALLETS)} autoConnect>
+    <WalletManager />
     {children}
   </AptosWalletProvider>
 );
